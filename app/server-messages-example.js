c1dac3246025d1e0d5cbb30e4bfac4fd04e3d271
Marco de Jongh <marco.dejongh@gfk.com>
2015-09-06 18:27:45 +0200

    Updated example page

--- app/server-messages-example.js
+++ app/server-messages-example.js
@@ -1,23 +1,61 @@
+/* global serverMessages: true*/
+serverMessages = new ServerMessages();
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+  Template.generate.events({
+    'click .js-btn-client': function (event, template) {
+      var subject = template.find('.js-title').value,
+        message = template.find('.js-msg').value,
+        type = template.find('.js-level').value.toLowerCase(),
+        timeout = parseInt(template.find('.js-timeout').value, 10),
+        userCloseable = template.find('.js-closeable').checked;
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+      Meteor.call('notify', 'serverMessage:' + type, subject, message, {
+        userCloseable: userCloseable,
+        timeout: timeout
+      });
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.generate.helpers({
+    types: function () {
+      var returnValue = [];
+
+      _.each(Notifications.TYPES, function (i, k) {
+        returnValue.push({name:k, value: k});
+      });
+
+      return returnValue;
+    },
+    getSource: function () {
+      return window.location.origin;
     }
   });
+
+  serverMessages.listen('serverMessage:info', function (subject, message, options) {
+    Notifications.info(subject, message, options);
+  });
+
+  serverMessages.listen('serverMessage:warn', function (subject, message, options) {
+    Notifications.warn(subject, message, options);
+  });
+
+  serverMessages.listen('serverMessage:success', function (subject, message, options) {
+    Notifications.success(subject, message, options);
+  });
+
+  serverMessages.listen('serverMessage:error', function (subject, message, options) {
+    Notifications.error(subject, message, options);
+  });
+
 }
 
+
+
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Meteor.methods({
+    notify: function () {
+      serverMessages.notify.apply(serverMessages, arguments);
+    }
   });
 }
